a9685eb8efa027dbfb6dbeabc2378e3f08a9981b
dhust <rafaelmoura.dev@gmail.com>
2015-09-22 15:41:08 +0000

    convention name in chip8 class

--- Chip8.h
+++ Chip8.h
@@ -11,42 +11,47 @@ using namespace std;
 
 class Chip8
 {
-private:
-    bool drawFlag;
-    unsigned short opcode;
-    unsigned char memory[4096];
-    unsigned char V[16];
-    unsigned short I;
-    unsigned short pc;
-    unsigned char gfx[64 * 32];
-    unsigned char delay_timer;
-    unsigned char sound_timer;
-    unsigned short stack[16];
-    unsigned short sp;
-    unsigned char key[16];
-
-    bool InitGraphics();
-    bool InitSound();
-    bool InitInput();
-
-    //Coisas do SDL2
-    SDL_Window *window;
-    SDL_Renderer *rend;
-
-    SDL_Event event;
-
 public:
     Chip8();
-    bool InitSystems();
-    void EmulateCycle();
+    bool initSystems();
+    void emulateCycle();
     void drawGraphics();
     void setKeys();
     bool loadGame(const char *gamename);
     bool getDrawFlag() const;
     void setDrawFlag(bool value);
-    void Dispose();
-    bool WantToExit();
-    void Update();
+    void dispose();
+    bool wantToExit();
+    void update();
+
+private:
+    bool drawFlag_;
+    unsigned short opcode_;
+    unsigned char memory_[4096];
+    unsigned char V_[16];
+    unsigned short I_;
+    unsigned short p_c;
+    unsigned char gfx_[64 * 32];
+    unsigned char delayTimer;
+    unsigned char soundTimer;
+    unsigned short stack_[16];
+    unsigned short sp_;
+    unsigned char key_[16];
+
+    bool initGraphics();
+    bool initSound();
+    bool initInput();
+
+    //Coisas do SDL2
+	struct 
+	{
+    	SDL_Window *window;
+    	SDL_Renderer *rend;
+		SDL_Event event;
+	
+	} screen_;
+
+
 };
 
 #endif // CHIP8_H
