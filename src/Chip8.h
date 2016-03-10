b913ec7e4771d398f832361a2b39fe6214301450
dhust <rafaelmoura.dev@gmail.com>
2016-03-10 07:48:31 -0300

    added instruction table

--- src/Chip8.h
+++ src/Chip8.h
@@ -17,7 +17,7 @@ class Chip8
 	constexpr static long CHIP8_CLOCK_FREQUENCY = CLOCKS_PER_SEC / 60;
 	constexpr static size_t WIDTH = 64;
 	constexpr static size_t HEIGHT = 32;
-
+	friend class Chip8Instructions;
 public:
 	Chip8();
 	Chip8(const Chip8&) = delete;
